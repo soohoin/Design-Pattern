4013188d78a0e49302cde72a1b6f03174a8b7cc7
user-PC\user <00000>
2019-06-06 18:08:40 +0900

    Strategy Pattern README.md push

---
+++ src/strategy_pattern/README.md
@@ -0,0 +1,15 @@
+## Strategy pattern
+
+- 전략 패턴(strategy pattern) 또는 정책 패턴(policy pattern)은 실행 중에 알고리즘을 선택할 수 있게 하는 행위 디자인 패턴이다.
+- 프로그램을 확장 했을 때 변경될 알고리즘이 있다면 이 알고리즘을 클래스별로 캡슐화하고 필요할 때 교체할 수 있도록 한다.
+
+## 예시 (Strategy Pattern 적용 전)
+
+- 군인을 만들어서 구 군인에게 이름을 갖게하고 공격/이동 기능을 넣는다.
+- 군인은 여러 좋유의 군인이 있기에 Abstract 클레스(Millitary.java)로 정의한 후 여러 군인을 만들때 이를 상속 받아서 구현한다.
+- 우선 육군과 공군을 만들었다.
+  - 육군 - 공격:지상  이동:걸어서이동    
+  - 공군 - 공격:하늘  이동:비행기로이동    
+
+- Class Diagram
+![Strategy01](./Images/Strategy/Strategy01.PNG)
--- src/strategy_pattern/before/Client.java
+++ src/strategy_pattern/before/Client.java
@@ -5,12 +5,16 @@ public class Client {
 		Millitary army = new Army("홍길동");
 		Millitary airForoce = new AirForce("김동수");
 		
-		System.out.println("나는 육군의 " + army.getName()+ " 이다.");
+		System.out.println("나는 육군 " + army.getName()+ " 이다.");
+		System.out.print("attact() : ");
 		army.attact();
+		System.out.print("move() : ");
 		army.move();
 		
-		System.out.println("나는 육군의 " + airForoce.getName()+ " 이다.");
+		System.out.println("나는 공군 " + airForoce.getName()+ " 이다.");
+		System.out.print("attact() : ");
 		airForoce.attact();
+		System.out.print("move() : ");
 		airForoce.move();
 	}
 }
